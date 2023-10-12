84ee91532849ce08d31fdfffe857eddf8093df1a
sidhtc510 <sidhtc510@gmail.com>
2023-10-12 10:16:22 +0200

    ATLAS-6 bug fix

--- src/admin/components/AnatomicalStructureSubjectTableItem/index.tsx
+++ src/admin/components/AnatomicalStructureSubjectTableItem/index.tsx
@@ -3,12 +3,12 @@ import Button from "../../components/UI/Button";
 import s from "./s.module.css";
 
 const AnatomicalStructureSubjectTableItem = ({ id, name, color }) => {
-
-    
     return (
-        <tr style={{backgroundColor:`#${color}`}}>
+        <tr style={{ backgroundColor: `#${color}` }}>
             <td>
-                <Link to={`/admin/AnatomicalStructureSubject/${id}`} className={s.link}>{name}</Link>
+                <Link to={`/admin/AnatomicalStructureSubject/${id}`} className={s.link}>
+                    {name}
+                </Link>
             </td>
             <td>{color}</td>
             <td>
