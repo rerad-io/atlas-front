7534196794f18131ea5ca38d7f53519fa9b3eef4
Slava <v.zhevaga@gmail.com>
2023-10-13 01:39:01 +0200

    ATLAS-14 edit AnatomicalStructureEditPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast";
 import AnatomicalStructureListPage from "./admin/pages/AnatomicalStructureListPage";
 import AnatomicalStructureSubjectListPage from "./admin/pages/AnatomicalStructureSubjectListPage";
 import AnatomicalStructureSubjectEditPage from "./admin/pages/AnatomicalStructureSubjectEditPage";
-import AnatomicalStructureEditPage from "./admin/pages/AnatomicalStructureEditPage/AnatomicalStructureEditPage";
+import AnatomicalStructureEditPage from "./admin/pages/AnatomicalStructureEditPage";
 
 export default function App() {
     const routes = [
