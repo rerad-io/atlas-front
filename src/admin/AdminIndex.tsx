5848c1eeff2748b4fd372ea0abc6fe4cbf0dec03
Slava <v.zhevaga@gmail.com>
2023-10-17 13:53:05 +0200

    ATLAS-24 add component for current frame

--- src/admin/AdminIndex.tsx
+++ src/admin/AdminIndex.tsx
@@ -1,35 +1,10 @@
 import { NavLink } from "react-router-dom";
 import s from "./AdminIndex.module.scss";
-import StudySeriesItemGallery from "./components/StudySeriesItemGallery";
-import StudySeriesItemCurrentFrame from "./components/StudySeriesItemCurrentFrame";
-import { gallery_list } from "../data/data";
-import { useState } from "react";
 
 const AdminIndex = () => {
     const name = "Full name";
     const role = "Administrator";
 
-    // ======= Перенести после размещения StudySeriesItemGallery и StudySeriesItemCurrentFrame на нужную страницу
-    const galleryList: {
-        id: string;
-        img: string;
-        alt: string;
-    }[] = gallery_list;
-    const [currentFrame, setCurrentFrame] = useState(galleryList[0]);
-
-    const handleClick = (id: string) => {
-        const newFrame: {
-            id: string;
-            img: string;
-            alt: string;
-        } = galleryList.find((elem) => elem.id === id) as {
-					id: string;
-					img: string;
-					alt: string;
-			};
-        setCurrentFrame(newFrame);
-    };
-		//==========
     return (
         <div className={s.page}>
             <section className={s.menu_section}>
@@ -63,38 +38,6 @@ const AdminIndex = () => {
                     </div>
                 </div>
             </section>
-						{/*Для страницы Серии ==============================*/}
-            <StudySeriesItemGallery galleryList={galleryList} handleClick={handleClick} />
-            <section
-						className={s.frame_info}
-						style={{
-							padding: "20px 0",
-						}}>
-                <div className="container">
-                    <div className={s.frame_wrapper}
-										style={{
-											width:"100%",
-											display: "flex",
-											alignItems: "flex-start",
-											gap:"20px",
-										}}>
-                        <StudySeriesItemCurrentFrame currentFrame={currentFrame} />
-                        <div 
-													style={{
-														overflow:"hidden",
-													}}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci voluptatem corporis qui voluptate
-                            veritatis commodi iste est quis vero! Dolorem asperiores eos reprehenderit quisquam nam earum obcaecati
-                            repellendus commodi magnam, odio distinctio minima exercitationem ea doloremque labore voluptatibus. Error,
-                            nulla expedita omnis maiores veniam temporibus minima fuga tempora neque magnam iure illum, earum vel saepe,
-                            animi laudantium! Voluptatem, aut suscipit. Fugiat est, harum iste ipsam explicabo ullam? Dolor consequuntur,
-                            perferendis facilis veniam commodi cupiditate voluptatum rem ratione esse magni vel iste. Tempora sint culpa,
-                            quasi asperiores sit atque est magni explicabo et quam in ducimus commodi harum placeat veritatis.
-                        </div>
-                    </div>
-                </div>
-            </section>
-						{/*=============================*/}
         </div>
     );
 };
--- src/admin/pages/StudySeriesEditPage/index.tsx
+++ src/admin/pages/StudySeriesEditPage/index.tsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
 import s from "./s.module.css";
+import { useState } from "react";
 import Button from "../../../components/UI/Button";
+import { gallery_list } from "../../../data/data";
+import StudySeriesItemGallery from "../../components/StudySeriesItemGallery";
+import StudySeriesItemCurrentFrame from "../../components/StudySeriesItemCurrentFrame";
 
 const StudySeriesEditPage = () => {
     const { Id } = useParams<{ Id: string }>();
@@ -53,6 +57,29 @@ const StudySeriesEditPage = () => {
     // };
 
     // Id	Study	Number	Name	PreviewFrame	InstanceCount	SagitalFrame	CoronalFrame
+
+    // ======= Перенести после размещения StudySeriesItemGallery и StudySeriesItemCurrentFrame на нужную страницу
+    const galleryList: {
+        id: string;
+        img: string;
+        alt: string;
+    }[] = gallery_list;
+    const [currentFrame, setCurrentFrame] = useState(galleryList[0]);
+
+    const handleClick = (id: string) => {
+        const newFrame: {
+            id: string;
+            img: string;
+            alt: string;
+        } = galleryList.find((elem) => elem.id === id) as {
+            id: string;
+            img: string;
+            alt: string;
+        };
+        setCurrentFrame(newFrame);
+    };
+    //==========
+
     return (
         <div className={s.page}>
             <h1>Редактирование Серии Исследования</h1>
@@ -80,6 +107,43 @@ const StudySeriesEditPage = () => {
 
                 <Button>Save</Button>
             </form>
+
+            {/*Для страницы Серии ==============================*/}
+            <StudySeriesItemGallery galleryList={galleryList} handleClick={handleClick} />
+            <section
+                className={s.frame_info}
+                style={{
+                    padding: "20px 0",
+                }}
+            >
+                <div className="container">
+                    <div
+                        className={s.frame_wrapper}
+                        style={{
+                            width: "100%",
+                            display: "flex",
+                            alignItems: "flex-start",
+                            gap: "20px",
+                        }}
+                    >
+                        <StudySeriesItemCurrentFrame currentFrame={currentFrame} />
+                        <div
+                            style={{
+                                overflow: "hidden",
+                            }}
+                        >
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed adipisci voluptatem corporis qui voluptate
+                            veritatis commodi iste est quis vero! Dolorem asperiores eos reprehenderit quisquam nam earum obcaecati
+                            repellendus commodi magnam, odio distinctio minima exercitationem ea doloremque labore voluptatibus. Error,
+                            nulla expedita omnis maiores veniam temporibus minima fuga tempora neque magnam iure illum, earum vel saepe,
+                            animi laudantium! Voluptatem, aut suscipit. Fugiat est, harum iste ipsam explicabo ullam? Dolor consequuntur,
+                            perferendis facilis veniam commodi cupiditate voluptatum rem ratione esse magni vel iste. Tempora sint culpa,
+                            quasi asperiores sit atque est magni explicabo et quam in ducimus commodi harum placeat veritatis.
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/*=============================*/}
         </div>
     );
 };
