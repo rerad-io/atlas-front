a3e1d5e80fee24d329b898fc1de23828ff045a7f
Slava <v.zhevaga@gmail.com>
2023-11-16 14:23:17 +0100

    ATLAS-73 fix study create

--- src/admin/pages/StudyEditPage/index.tsx
+++ src/admin/pages/StudyEditPage/index.tsx
@@ -5,14 +5,22 @@ import { createStudy, getStudyId, updateStudy } from "../../../requests/StudyReq
 import Button from "../../../components/UI/Button";
 import StudySeriesList from "../../components/StudySeriesList";
 import s from "./styles.module.scss";
+import { Study } from "../../../_types";
 
 const StudyEditPage = () => {
     const navigate = useNavigate();
     const { id } = useParams<{ id: string }>();
     const formRef = useRef<HTMLFormElement | null>(null);
+		
+    const [study, setStudy] = useState<Study>();
     const [createAnother, setCreateAnother] = useState(false);
-
-    const [study, setStudy] = useState();
+		const [values, setValues] = useState<any>({
+			externalId: "",
+			name: "",
+			description: "",
+			previewFrame: "",
+		});
+	
 
     useEffect(() => {
         if (id) {
@@ -30,19 +38,11 @@ const StudyEditPage = () => {
 
     const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const obj: Record<string, string> = {};
-
-        formData.forEach((value, key) => {
-            if (key !== "createAnother" && key !== "externalId") {
-                obj[key] = value as string;
-            }
-        });
-
+	
         if (id) {
             const fetchDataAndUpdateStudy = async () => {
                 try {
-                    const updatedObj = await updateStudy(obj, id);
+                    const updatedObj = await updateStudy(values, id);
                     setStudy(updatedObj);
                     toast.success("Study updated!");
                 } catch (error) {
@@ -54,7 +54,7 @@ const StudyEditPage = () => {
         } else {
             const fetchDataAndCreateStudy = async () => {
                 try {
-                    const data = await createStudy(obj);
+                    const data = await createStudy(values);
                     if (data) {
                         if (!createAnother) {
                             navigate(`/admin/Study`);
@@ -77,43 +77,60 @@ const StudyEditPage = () => {
         }
     };
 
+		const handleChange = (e: React.FormEvent<HTMLInputElement| HTMLTextAreaElement>) => {
+			setValues({ ...values, [e.target.name]: e.target.value });
+		};
+	
+
     return (
         <div className={s.page}>
             <h1 className="title">{id ? `Редактирование` : `Создание`} Исследования</h1>
             <form ref={formRef} onSubmit={onSubmitHandler} className={s.form}>
                 {
-                    <label htmlFor="externalId">
+                    <label >
                         External Id:
                         <input
                             required
                             type="text"
                             name="externalId"
-                            id="externalId"
-                            defaultValue={study?.externalId}
-                            placeholder={study?.externalId}
+                            value={values[oninput?.name]}
+                            placeholder={study ? study.externalId : ""}
                             style={{ width: "300px" }}
+														onChange={handleChange}
                         />
                     </label>
                 }
-                <label htmlFor="studyName">
+                <label >
                     Study Name:
-                    <input required type="text" name="name" id="studyName" defaultValue={study?.name} />
+                    <input 
+										required 
+										type="text" 
+										name="name"  
+										defaultValue={study?.name}
+										onChange={handleChange} />
                 </label>
-                <label htmlFor="studyDescription">
+                <label >
                     Study Description:
-                    <textarea id="studyDescription" name="description" rows={4} cols={50} defaultValue={study?.description}></textarea>
+                    <textarea  
+										name="description" 
+										rows={4} cols={50} 
+										defaultValue={study?.description}
+										onChange={handleChange}/>
                 </label>
-                <label htmlFor="previewFrame">
+                <label >
                     Preview Frame:
-                    <input type="text" name="previewFrame" id="previewFrame" defaultValue={study?.previewFrame} />
+                    <input 
+										type="text" 
+										name="previewFrame"  
+										defaultValue={study?.previewFrame}
+										onChange={handleChange} />
                 </label>
                 {typeof id === "undefined" ? (
-                    <label htmlFor="createAnother">
+                    <label >
                         Create Another:
                         <input
                             type="checkbox"
                             name="createAnother"
-                            id="createAnother"
                             defaultChecked={createAnother}
                             onChange={() => {
                                 setCreateAnother(!createAnother);
